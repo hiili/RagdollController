9b219f89580c2d901d3dfac7b20b0990a3d6684d
Paul Wagner <paul.wagner@iki.fi>
2015-01-18 14:26:13 +0200

    Owen goes to Sun Temple and UE 4.6.1: Migrated all RagdollController material to UE 4.6.1 and the Sun Temple demo map.
    Config:
      - Configuration files not yet imported (are the defaults from Sun Temple at the moment)
    
    Content:
      - Re-imported Owen from Content Examples 4.6.1
      - OwenBP.uasset and BasisVectorsBP.uasset dropped
    
    Saved:
      - Entire folder dropped
    
    Source:
      - Created VS files and the Source folder by creating a new, empty GameMode class
      - Copied over all RagdollController source files (not yet renamed nor merged anything)
    
    utility/root:
      - Shortcuts updated
      - Added stuff for managing shortcuts

---
+++ manage_shortcuts.bat
@@ -0,0 +1,19 @@
+REM An example script for managing the shortcuts
+REM
+REM You might need to run this twice or more, as each run replaces only the first occurence
+
+lnkedit.exe -tda 'launch editor debug client.lnk' RagdollController45 RagdollController
+lnkedit.exe -tda 'launch editor debug listen server.lnk' RagdollController45 RagdollController
+lnkedit.exe -tda 'launch editor debug server.lnk' RagdollController45 RagdollController
+lnkedit.exe -tda 'launch editor debug standalone.lnk' RagdollController45 RagdollController
+lnkedit.exe -tda 'launch editor debug.lnk' RagdollController45 RagdollController
+lnkedit.exe -tda 'launch editor debuggame client.lnk' RagdollController45 RagdollController
+lnkedit.exe -tda 'launch editor debuggame listen server.lnk' RagdollController45 RagdollController
+lnkedit.exe -tda 'launch editor debuggame server.lnk' RagdollController45 RagdollController
+lnkedit.exe -tda 'launch editor debuggame standalone.lnk' RagdollController45 RagdollController
+lnkedit.exe -tda 'launch editor debuggame.lnk' RagdollController45 RagdollController
+lnkedit.exe -tda 'launch editor development client.lnk' RagdollController45 RagdollController
+lnkedit.exe -tda 'launch editor development listen server.lnk' RagdollController45 RagdollController
+lnkedit.exe -tda 'launch editor development server.lnk' RagdollController45 RagdollController
+lnkedit.exe -tda 'launch editor development standalone.lnk' RagdollController45 RagdollController
+lnkedit.exe -tda 'launch editor.lnk' RagdollController45 RagdollController
