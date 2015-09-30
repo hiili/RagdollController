94744ab0e407fe89eb9e1dc1889b993e9552ad2a
Paul Wagner <paul.wagner@iki.fi>
2015-09-30 16:08:36 +0300

    DeepSnapshotSystem: Create empty classes for SkeletalMeshComponent snapshots

---
+++ Source/RagdollController/DeepSnapshotSystem/PrimitiveComponentSnapshot.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "DeepSnapshotSystem/DeepSnapshotBase.h"
+#include "PrimitiveComponentSnapshot.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RAGDOLLCONTROLLER_API UPrimitiveComponentSnapshot : public UDeepSnapshotBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
