6595b5ffc021fdb12ef0792330cd2b43f57487c3
Paul Wagner <paul.wagner@iki.fi>
2015-10-16 16:52:15 +0300

    SkeletalMeshComponentSnapshot: Finish serialization code

--- Source/RagdollController/DeepSnapshotSystem/SkeletalMeshComponentSnapshot.cpp
+++ Source/RagdollController/DeepSnapshotSystem/SkeletalMeshComponentSnapshot.cpp
@@ -3,28 +3,10 @@
 #include "RagdollController.h"
 #include "SkeletalMeshComponentSnapshot.h"
 
+#include "PhysicsPublic.h"
+#include "PhysXPublic.h"
+
 #include "Utility.h"
-
-#include <extensions/PxD6Joint.h>
-#include <PxRigidBody.h>
-#include <PxRigidDynamic.h>
-#include <PxTransform.h>
-
-
-
-
-static FArchive & operator<<(FArchive & archive, physx::PxTransform & data)
-{
-	archive.Serialize( &data, sizeof(data) );
-	return archive;
-}
-
-
-static FArchive & operator<<(FArchive & archive, physx::PxVec3 & data)
-{
-	archive.Serialize( &data, sizeof(data) );
-	return archive;
-}
 
 
 
@@ -32,6 +14,8 @@ static FArchive & operator<<(FArchive & archive, physx::PxVec3 & data)
 void USkeletalMeshComponentSnapshot::SerializeTarget( FArchive & archive, UActorComponent & target )
 {
 	Super::SerializeTarget( archive, target );
+
+	check( archive.IsSaving() || archive.IsLoading() );
 
 	// downcast the target, log and return if wrong type
 	USkeletalMeshComponent * skeletalMeshTarget = dynamic_cast<USkeletalMeshComponent *>(&target);
@@ -41,72 +25,65 @@ void USkeletalMeshComponentSnapshot::SerializeTarget( FArchive & archive, UActor
 		return;
 	}
 
-	// check the number of bones in target
+	// check the number of bodies in target
 	int32 numBodies = skeletalMeshTarget->Bodies.Num();
 
-	if( archive.IsSaving() )
+	// sync the number of bodies with archive; numBodies is re-assigned if loading
+	archive << numBodies;
+
+	// if recalling, then check that the number of bodies still matches our target (for example, one might not be initialized yet)
+	if( archive.IsLoading() && numBodies != skeletalMeshTarget->Bodies.Num() )
 	{
-		// snapshot: serialize
-
-		// store the number of bones
-		archive << numBodies;
-
-		// loop through bones and store their state data
-		for( int body = 0; body < numBodies; ++body )
-		{
-			physx::PxRigidDynamic * pxBody = skeletalMeshTarget->Bodies[body]->GetPxRigidDynamic();
-			if( !pxBody )
-			{
-				UE_LOG( LogDeepSnapshotSystem, Error, TEXT( "(%s) GetPxRididDynamic() failed for body %d!" ), TEXT( __FUNCTION__ ), body );
-				UE_LOG( LogDeepSnapshotSystem, Error, TEXT( "%s" ), *LogCreateDiagnosticLine() );
-				return;
-			}
-
-			// Copy pose from the skeletal mesh to archive
-			archive << Utility::as_lvalue( pxBody->getGlobalPose() );
-			archive << Utility::as_lvalue( pxBody->getLinearVelocity() );
-			archive << Utility::as_lvalue( pxBody->getAngularVelocity() );
-		}
+		UE_LOG( LogDeepSnapshotSystem, Error, TEXT( "(%s) Number of bones do not match. Cannot recall!" ), TEXT( __FUNCTION__ ) );
+		UE_LOG( LogDeepSnapshotSystem, Error, TEXT( "%s" ), *LogCreateDiagnosticLine() );
+		return;
 	}
-	else   /////////////////// MERGE 'EM!
+
+	// lock the scene and loop through bodies
+	check( GetWorld() && GetWorld()->GetPhysicsScene() && GetWorld()->GetPhysicsScene()->GetPhysXScene( PST_Sync ) );
+	SCOPED_SCENE_READ_LOCK( GetWorld()->GetPhysicsScene()->GetPhysXScene( PST_Sync ) );
+	for( int body = 0; body < numBodies; ++body )
 	{
-		// recall: deserialize
-
-		// read the number of bones in archive
-		int32 numBodiesIncoming;
-		archive << numBodiesIncoming;
-
-		// verify that the skeletal meshes have the same number of bones (for example, one might not be initialized yet)
-		if( numBodiesIncoming != numBodies )
+		// get the physx handle for this body
+		physx::PxRigidDynamic * pxBody = skeletalMeshTarget->Bodies[body]->GetPxRigidDynamic_AssumesLocked();
+		if( !pxBody )
 		{
-			UE_LOG( LogDeepSnapshotSystem, Error, TEXT( "(%s) Number of bones do not match. Cannot perform the recall!" ), TEXT( __FUNCTION__ ) );
+			UE_LOG( LogDeepSnapshotSystem, Error, TEXT( "(%s) GetPxRididDynamic() failed for body %d!" ), TEXT( __FUNCTION__ ), body );
 			UE_LOG( LogDeepSnapshotSystem, Error, TEXT( "%s" ), *LogCreateDiagnosticLine() );
 			return;
 		}
 
-		// Loop through bones and apply data from storage to each
-		for( int body = 0; body < numBodies; ++body )
+		// Sync the state of the current body with archive
+		if( archive.IsSaving() )
 		{
-			physx::PxRigidDynamic * pxBody = skeletalMeshTarget->Bodies[body]->GetPxRigidDynamic();
-			if( !pxBody )
-			{
-				UE_LOG( LogDeepSnapshotSystem, Error, TEXT( "(%s) GetPxRididDynamic() failed for body %d!" ), TEXT( __FUNCTION__ ), body );
-				UE_LOG( LogDeepSnapshotSystem, Error, TEXT( "%s" ), *LogCreateDiagnosticLine() );
-				return;
-			}
-
-			// Copy pose from archive to the skeletal mesh
+			// snapshot: serialize
 
 			physx::PxTransform t;
 			physx::PxVec3 v;
 
-			archive << t;
+			t = pxBody->getGlobalPose();
+			archive.Serialize( &t, sizeof(t) );
+
+			v = pxBody->getLinearVelocity();
+			archive.Serialize( &v, sizeof(v) );
+
+			v = pxBody->getAngularVelocity();
+			archive.Serialize( &v, sizeof(v) );
+		}
+		else
+		{
+			// recall: deserialize
+
+			physx::PxTransform t;
+			physx::PxVec3 v;
+
+			archive.Serialize( &t, sizeof(t) );
 			pxBody->setGlobalPose( t );
 
-			archive << v;
+			archive.Serialize( &v, sizeof(v) );
 			pxBody->setLinearVelocity( v );
 
-			archive << v;
+			archive.Serialize( &v, sizeof(v) );
 			pxBody->setAngularVelocity( v );
 		}
 	}
